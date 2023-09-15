990e230524d1f0fd091e16c756e9ad15b4238aec
myungsangBaek <bms6168@naver.com>
2023-09-15 15:18:20 +0900

    feat: Generate gravity

--- Firework/index.js
+++ Firework/index.js
@@ -21,12 +21,15 @@ class Canvas extends CanvasOption {
     this.createParticles();
   }
   createParticles() {
-    const PARTICLE_NUM = 2000;
+    const PARTICLE_NUM = 400;
     const x = randomNumBetween(0, this.canvasWidth);
     const y = randomNumBetween(0, this.canvasHeight);
     for (let i = 0; i < PARTICLE_NUM; i++) {
-      const vx = randomNumBetween(-5, 5);
-      const vy = randomNumBetween(-5, 5);
+      const r = randomNumBetween(0, 3);
+      const angle = (Math.PI / 180) * randomNumBetween(0, 360);
+
+      const vx = r * Math.cos(angle);
+      const vy = r * Math.sin(angle);
       this.particles.push(new Particle(x, y, vx, vy));
     }
   }
--- Firework/js/Particle.js
+++ Firework/js/Particle.js
@@ -8,17 +8,22 @@ export default class Particle extends CanvasOption {
     this.vx = vx;
     this.vy = vy;
     this.opacity = 1;
+    this.gravity = 0.12;
+    this.friction = 0.93;
   }
   update() {
+    this.vy += this.gravity;
+    this.vx *= this.friction;
+    this.vy *= this.friction;
     this.x += this.vx;
     this.y += this.vy;
-    this.opacity -= 0.001;
+    this.opacity -= 0.01;
   }
 
   draw() {
     this.ctx.fillStyle = `rgba(255,255,255,${this.opacity})`;
     this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
     this.ctx.fill();
     this.ctx.closePath();
   }
